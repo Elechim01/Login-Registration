c330ce8cb6d52d23554e3df326428097acf6fedf
Elechim01 <mikymanny015@gmail.com>
2020-12-27 13:23:29 +0100

    Initial Commit

---
+++ Login+Registration/Login_RegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_RegistrationApp.swift
+//  Login+Registration
+//
+//  Created by Michele on 27/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Login_RegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
